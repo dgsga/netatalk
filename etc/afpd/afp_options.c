c257f3ad8475cb4b6f21872902c1a1bee4254693
dgsga <181612+dgsga@users.noreply.github.com>
2022-05-22 15:37:47 +0100

    Remove afppasswd and cracklib

--- etc/afpd/afp_options.c
+++ etc/afpd/afp_options.c
@@ -110,13 +110,6 @@ static void show_version_extended(void )
 	puts( "No" );
 #endif
 
-	printf( "      cracklib support:\t" );
-#ifdef USE_CRACKLIB
-	puts( "Yes" );
-#else
-	puts( "No" );
-#endif
-
 	printf( "            EA support:\t" );
 	puts( EA_MODULES );
 
