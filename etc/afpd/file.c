36c439eddaa9d56189d1e833b708191b6381d69c
dgsga <181612+dgsga@users.noreply.github.com>
2023-08-13 11:38:18 +0100

    Revert "add handling for cases where ad_entry() returns NULL"
    This reverts commit 9d0c21298363e8174cdfca657e66c4d10819507b.

--- etc/afpd/file.c
+++ etc/afpd/file.c
@@ -273,7 +273,6 @@ int getmetadata(const AFPObj *obj,
 {
     char		*data, *l_nameoff = NULL, *upath;
     char                *utf_nameoff = NULL;
-    char		*ade = NULL;
     int			bit = 0;
     uint32_t		aint;
     cnid_t              id = 0;
@@ -475,10 +474,7 @@ int getmetadata(const AFPObj *obj,
             }
             else {
                 if ( adp ) {
-                    ade = ad_entry(adp, ADEID_FINDERI);
-                    AFP_ASSERT(ade != NULL);
-
-                    memcpy(fdType, ade, 4);
+                    memcpy(fdType, ad_entry( adp, ADEID_FINDERI ), 4 );
 
                     if ( memcmp( fdType, "TEXT", 4 ) == 0 ) {
                         achar = '\x04';
@@ -557,19 +553,8 @@ int getmetadata(const AFPObj *obj,
                10.3 clients freak out. */
 
     	    aint = st->st_mode;
-            /*
-             * ad_open() does not initialize adouble header
-             * for symlinks. Hence this should be skipped to
-             * avoid AFP_ASSERT here. Decision was made to
-             * not alter ad_open() behavior so that
-             * improper ops on symlink adoubles will be
-             * more visible (assert).
-             */
-            if (adp && (ad_meta_fileno(adp) != AD_SYMLINK)) {
-                ade = ad_entry(adp, ADEID_FINDERI);
-                AFP_ASSERT(ade != NULL);
-
-	        memcpy(fdType, ade, 4);
+ 	    if (adp) {
+	        memcpy(fdType, ad_entry( adp, ADEID_FINDERI ), 4 );
                 if ( memcmp( fdType, "slnk", 4 ) == 0 ) {
 	 	    aint |= S_IFLNK;
             	}
@@ -831,7 +816,6 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
     struct extmap	*em;
     int			bit, isad = 1, err = AFP_OK;
     char                *upath;
-    char		*ade = NULL;
     u_char              achar, *fdType, xyy[4]; /* uninitialized, OK 310105 */
     uint16_t		ashort, bshort, oshort;
     uint32_t		aint;
@@ -1035,10 +1019,8 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
             ad_setdate(adp, AD_DATE_BACKUP, bdate);
             break;
         case FILPBIT_FINFO :
-            ade = ad_entry(adp, ADEID_FINDERI);
-            AFP_ASSERT(ade != NULL);
-            if (default_type(ade)
-                    && (
+            if (default_type( ad_entry( adp, ADEID_FINDERI ))
+                    && ( 
                      ((em = getextmap( path->m_name )) &&
                       !memcmp(finder_buf, em->em_type, sizeof( em->em_type )) &&
                       !memcmp(finder_buf + 4, em->em_creator,sizeof( em->em_creator)))
@@ -1048,7 +1030,7 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
             )) {
                 memcpy(finder_buf, ufinderi, 8 );
             }
-            memcpy(ade, finder_buf, 32 );
+            memcpy(ad_entry( adp, ADEID_FINDERI ), finder_buf, 32 );
             break;
         case FILPBIT_UNIXPR :
             if (upriv_bit) {
@@ -1056,12 +1038,9 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
             }
             break;
         case FILPBIT_PDINFO :
-            ade = ad_entry(adp, ADEID_FINDERI);
-            AFP_ASSERT(ade != NULL);
-
             if (obj->afp_version < 30) { /* else it's UTF8 name */
-                memcpy(ade, fdType, 4 );
-                memcpy(ade + 4, "pdos", 4 );
+                memcpy(ad_entry( adp, ADEID_FINDERI ), fdType, 4 );
+                memcpy(ad_entry( adp, ADEID_FINDERI ) + 4, "pdos", 4 );
                 break;
             }
             /* fallthrough */
