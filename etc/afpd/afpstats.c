220f28f8c804226cf44e6e42355209d2fb952dd8
dgsga <181612+dgsga@users.noreply.github.com>
2024-06-29 08:59:56 +0100

    Migrate afpstats from dbus-glib to GDBus

--- etc/afpd/afpstats.c
+++ etc/afpd/afpstats.c
@@ -22,8 +22,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <dbus/dbus-glib.h>
 #include <glib.h>
+#include <gio/gio.h>
 
 #include <atalk/compat.h>
 #include <atalk/errchk.h>
@@ -31,18 +31,19 @@
 #include <atalk/server_child.h>
 
 #include "afpstats_obj.h"
-#include "afpstats_service_glue.h"
+//#include "afpstats_service_glue.h"
 
 /*
  * Beware: this struct is accessed and modified from the main thread
  * and from this thread, thus be careful to lock and unlock the mutex.
  */
 static server_child_t *childs;
+static GDBusNodeInfo *introspection_data = NULL;
 
 static gpointer afpstats_thread(gpointer _data)
 {
-    DBusGConnection *bus;
-    DBusGProxy *bus_proxy;
+    GDBusConnection *bus;
+    GDBusProxy *bus_proxy;
     GError *error = NULL;
     GMainContext *ctxt;
     GMainLoop *thread_loop;
@@ -56,32 +57,57 @@ static gpointer afpstats_thread(gpointer _data)
     ctxt = g_main_context_new();
     thread_loop = g_main_loop_new(ctxt, FALSE);
 
-    dbus_g_object_type_install_info(AFPSTATS_TYPE_OBJECT, &dbus_glib_afpstats_obj_object_info);
-
-    if (!(bus = dbus_g_bus_get_private(DBUS_BUS_SYSTEM, ctxt, &error))) {
+    if (!(bus = g_bus_get_sync(G_BUS_TYPE_SYSTEM,
+                               NULL,
+                               &error))) {
         LOG(log_error, logtype_afpd,"Couldn't connect to system bus: %s", error->message);
         return NULL;
     }
 
-    if (!(bus_proxy = dbus_g_proxy_new_for_name(bus, "org.freedesktop.DBus",
-                                                "/org/freedesktop/DBus",
-                                                "org.freedesktop.DBus"))) {
+    if (!(bus_proxy = g_dbus_proxy_new_sync(bus,
+                                            G_DBUS_PROXY_FLAGS_NONE,
+                                            NULL, /* GDBusInterfaceInfo */
+                                            "org.freedesktop.DBus",
+                                            "/org/freedesktop/DBus",
+                                            "org.freedesktop.DBus",
+                                            NULL, /* GCancellable */
+                                            &error))) {
         LOG(log_error, logtype_afpd,"Couldn't create bus proxy");
         return NULL;
     }
 
-    if (!dbus_g_proxy_call(bus_proxy, "RequestName", &error,
-                           G_TYPE_STRING, "org.netatalk.AFPStats",
-                           G_TYPE_UINT, 0,
-                           G_TYPE_INVALID,
-                           G_TYPE_UINT, &request_name_result,
-                           G_TYPE_INVALID)) {
+    if (!g_dbus_proxy_call_sync(bus_proxy,
+                                "RequestName",
+                                g_variant_new("(as)", "org.netatalk.AFPStats", 0),
+                                G_DBUS_CALL_FLAGS_NONE,
+                                -1, /* timeout */
+                                NULL,
+                                &error)) {
         LOG(log_error, logtype_afpd, "Failed to acquire DBUS name: %s", error->message);
         return NULL;
     }
 
+    static const gchar introspection_xml[] =
+        "<node>"
+        "  <interface name='org.netatalk.AFPStats'>"
+        "    <method name='GetUsers'>"
+        "      <arg name='ret' type='as' direction='out'/>"
+        "    </method>"
+        "  </interface>"
+        "</node>";
+
+    introspection_data = g_dbus_node_info_new_for_xml (introspection_xml, NULL);
+    g_assert (introspection_data != NULL);
+
     AFPStatsObj *obj = g_object_new(AFPSTATS_TYPE_OBJECT, NULL);
-    dbus_g_connection_register_g_object(bus, "/org/netatalk/AFPStats", G_OBJECT(obj));
+
+    g_dbus_connection_register_object(bus,
+                                      "/org/netatalk/AFPStats",
+                                      introspection_data->interfaces[0],
+                                      NULL,
+                                      G_OBJECT(obj),
+                                      NULL,
+                                      &error);
 
     g_main_loop_run(thread_loop);
     return thread_loop;
